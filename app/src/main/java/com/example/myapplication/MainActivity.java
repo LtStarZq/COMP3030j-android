2e2f5b3ab2549fb14a8ab63e523116a0c08f4258
LtStarZq <ziqi.chen@ucdconnect.ie>
2020-05-26 15:20:25 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=(WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //确保跳转到另一个网页时仍然在当前WebView显示
+        webView.loadUrl("http://49.235.17.213/");
+    }
+}
